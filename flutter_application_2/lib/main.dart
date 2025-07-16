6e89af2929edd9790fe84a7f9ed80d5a9ef4e005
eszter-with-a-z <eszter.with.a.z@gmail.com>
2025-07-16 16:41:52 +0200

    Add code (all-in-one, oops)

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,112 +12,122 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My To-Do List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ListPage(title: '✨My To-Do List 📝'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ListPage extends StatefulWidget {
+  const ListPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ListPage> createState() => _ListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ListPageState extends State<ListPage> {
+// Variables
+  // Purpose: Freeze text field on submit button tap and store text
+  String _newTask = '';
+  // Purpose: store tasks
+  final List<String> _tasks=[];
+  // Purpose: constantly store current value of text field
+  final TextEditingController _controller = TextEditingController();
 
-  void _incrementCounter() {
+// Funtions
+  // For Add button: Add new task to tasks
+  void _addNewTask() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      //Freeze input field value
+      _newTask = _controller.text.trim();
+      if (_newTask != ''){
+        // If it is not empty, add new task to tasks
+        _tasks.add(_newTask);
+        // Empty new task and erase text field content
+        _newTask='';
+        _controller.clear(); 
+      }
+    });
+  }
+  // For tapping on List Tiles, delete tile + add to text field
+  void _deleteTask(int index){
+    setState(() {
+      // Update Text Field
+      _controller.text = _tasks[index];
+      // Remove from tasks
+      _tasks.remove(_tasks[index]);
     });
   }
 
+  // UI
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        // Build on context color
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding:  const EdgeInsets.all(32),
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+            const Text('What are you getting to do today?'),
+            
+            TextField(
+              // COnnect text field to controller so that
+              // you can use its methods
+              controller: _controller,
+              textAlign: TextAlign.center,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+                        
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: _addNewTask,
+              child: const Icon(Icons.add_circle),
+            ),
+
+            const SizedBox(height: 8),
+            // Since you are in Column:
+            // Add expanded to ListView to constrain it
+            Expanded(
+              child: ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                return Row(
+                  children: [
+                    // Since you are in Row:
+                    // Add Expanded to ListTile to contrain it
+                    Expanded(
+                      child:ListTile(
+                        title: Text(_tasks[index]),
+                        onTap: ()=> _deleteTask(index),
+                        ),
+                    ),
+                    Icon(
+                      Icons.cancel,
+                      color:Theme.of(context).colorScheme.inversePrimary
+                    )
+                  ],
+                );
+                
+              },
+              separatorBuilder: (_, __) => const Divider(), 
+              itemCount: _tasks.length),
+              )
+            
+            ]
+          ),
+        )
+      
     );
   }
 }
